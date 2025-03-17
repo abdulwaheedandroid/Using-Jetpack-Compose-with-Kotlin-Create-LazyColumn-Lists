f9bd887dc3d438b61a3f10c617d33179e7f86e3b
abdulwaheed-cp <abdul.waheed@cloudprimero.com>
2025-03-18 02:34:41 +0500

    creation scrollable list done

--- app/src/main/java/com/seasonin/lazycolumnlistwithanimation/MainActivity.kt
+++ app/src/main/java/com/seasonin/lazycolumnlistwithanimation/MainActivity.kt
@@ -4,28 +4,57 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.seasonin.lazycolumnlistwithanimation.ui.theme.LazycolumnlistwithanimationTheme
 
 class MainActivity : ComponentActivity() {
+    private val animals = listOf(
+        "Lion", "Tiger", " Elephant", "Giraffe",
+        "Starfish" +
+                "JellyFish",
+        "Eagle", "Dolphin", "Shark", "Whale", "Walrus", "Lion", "Tiger", " Elephant", "Giraffe",
+        "Starfish" +
+                "JellyFish",
+        "Eagle", "Dolphin", "Shark", "Whale", "Walrus",
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LazycolumnlistwithanimationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   AnimalList(animals = animals, modifier = Modifier.padding(innerPadding))
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun AnimalRow(animal: String) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(text = animal, Modifier
+            .fillMaxSize()
+            .padding(16.dp))
+    }
+}
+
+@Composable
+fun AnimalList(animals: List<String>, modifier: Modifier) {
+    LazyColumn {
+        items(animals) { animal ->
+            AnimalRow(animal = animal)
         }
     }
 }
