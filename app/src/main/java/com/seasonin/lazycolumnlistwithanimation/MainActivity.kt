64b5af51fc1fbf73b9bcc6ed6d699f09df96e080
abdulwaheed-cp <abdul.waheed@cloudprimero.com>
2025-03-30 04:35:53 +0500

    Different animations

--- app/src/main/java/com/seasonin/lazycolumnlistwithanimation/MainActivity.kt
+++ app/src/main/java/com/seasonin/lazycolumnlistwithanimation/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,9 +18,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -75,12 +84,17 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavHost(navController, startDestination = "animal_list") {
                         composable("animal_list") {
-                            AnimalList(animals = animals,navController, modifier = Modifier.padding(innerPadding))
+                            AnimalListSlideIn(
+                                animals = animalColors,
+                                navController,
+                                modifier = Modifier.padding(innerPadding)
+                            )
                         }
                         composable("animal_details/{animal}") { navBackStackEntry ->
                             val animal = navBackStackEntry.arguments?.getString("animal")
-                            if(animal != null) {
-                                AnimalDetails(animal)
+                            val color = animalColors[animal]?: Color.White
+                            if (animal != null) {
+                                AnimalDetails(animal, color)
                             }
                         }
                     }
@@ -91,34 +105,92 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AnimalRow(animal: String, navController: NavHostController) {
+fun AnimalRow(animal: String,color: Color? = null, navController: NavHostController) {
     val context = LocalContext.current
     var text by remember { mutableStateOf("Click me") }
-    Box(modifier = Modifier.fillMaxSize()
+    val animalColor = color ?: Color.LightGray
+    Box(modifier = Modifier
+        .fillMaxSize()
         .clickable {
             navController.navigate("animal_details/$animal")
-        }) {
-        Text(text = animal, Modifier
-            .fillMaxSize()
-            .padding(16.dp))
+        }
+        .background(animalColor)) {
+        Text(
+            text = animal, Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        )
 
     }
 }
 
 @Composable
-fun AnimalDetails(animal: String) {
+fun AnimalDetails(animal: String, initialColor: Color) {
+    val colors = listOf(Color.Red, Color.Green, Color.Blue, Color.Yellow, Color.Cyan, Color.Magenta)
+    var currentColor by remember { mutableStateOf(initialColor) }
+    val animatedColor by animateColorAsState(targetValue = currentColor, animationSpec = tween(2000))
     Column {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxSize().background(animatedColor)) {
             Text(text = "Detail page for $animal", modifier = Modifier.align(Alignment.Center))
+            Button(
+                onClick = {
+                    currentColor = colors.random()
+                }, modifier = Modifier.align(Alignment.BottomCenter).padding(16.dp)) {
+                Text("Change Color")
+            }
         }
     }
 }
 
 @Composable
-fun AnimalList(animals: List<String>, navController: NavHostController, modifier: Modifier) {
+fun AnimalList(animals: Map<String, Color>, navController: NavHostController, modifier: Modifier) {
     LazyColumn {
-        items(animals) { animal ->
-            AnimalRow(animal = animal, navController)
+        items(animals.keys.toList()) { animal ->
+            AnimalRow(animal = animal, color = null ,navController)
+        }
+    }
+}
+
+@Composable
+fun AnimalListCrossFade(animals: Map<String, Color>, navController: NavHostController, modifier: Modifier) {
+    LazyColumn {
+        items(animals.keys.toList()) { animal ->
+            val color = Color.LightGray
+            var isVisible by remember { mutableStateOf(false) }
+
+            LaunchedEffect(key1 = animal) {
+                isVisible = true
+            }
+
+            AnimatedVisibility(visible = isVisible,
+                enter = fadeIn(animationSpec = tween(2000)),
+                exit = fadeOut(animationSpec = tween(2000))
+            ) {
+                AnimalRow(animal = animal, color = null ,navController)
+            }
+
+        }
+    }
+}
+
+@Composable
+fun AnimalListSlideIn(animals: Map<String, Color>, navController: NavHostController, modifier: Modifier) {
+    LazyColumn {
+        items(animals.keys.toList()) { animal ->
+            val color = Color.LightGray
+            var isVisible by remember { mutableStateOf(false) }
+
+            LaunchedEffect(key1 = animal) {
+                isVisible = true
+            }
+
+            AnimatedVisibility(visible = isVisible,
+                enter = slideInHorizontally (initialOffsetX = {-1000}, animationSpec = tween(2000)),
+                exit = fadeOut(animationSpec = tween(2000))
+            ) {
+                AnimalRow(animal = animal, color = null ,navController)
+            }
+
         }
     }
 }
